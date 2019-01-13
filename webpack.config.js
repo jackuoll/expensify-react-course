e9508d1178de865e017d1ef63f0f6d3dd82391b6
Jack Ward <jack.ward@grabone.co.nz>
2019-01-14 11:28:06 +1300

    production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,33 @@ const path = require('path')
 
 const publicFolder = path.join(__dirname, 'public');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: publicFolder,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    },{
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
+module.exports = (env) => { 
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    output: {
+      path: publicFolder,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },{
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: publicFolder,
+      historyApiFallback: true
     }
-  ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: publicFolder,
-    historyApiFallback: true
-  }
-};
+  };
+}
