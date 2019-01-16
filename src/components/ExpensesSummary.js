aac22f974f1ff5ca7752d16158647abce669b22c
Jack Ward <jack.ward@grabone.co.nz>
2019-01-16 17:35:46 +1300

    style summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expensesTotal';
@@ -12,8 +13,13 @@ export const ExpensesSummary = (props) => {
   }
   const totalCost = numeral(getExpensesTotal(props.expenses) / 100).format('$0,0.00');
   return (
-    <div>
-      <h1>Viewing {quantity} {expenseWording} totalling {totalCost}.</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{quantity}</span> {expenseWording} totalling <span>{totalCost}</span>.</h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
